fb0e0bc0ddb56826662b0a19b9b905d13f2fcc7c
Guy Cole <guycole@gmail.com>
2016-05-04 03:04:43 +0000

    updated curl sample

--- curl.sh
+++ curl.sh
@@ -10,6 +10,8 @@
 #
 #curl -v http://52.39.56.135/api_gw_lab/demo1.php/6c5790e4-80e9-4ffe-b583-d8f50caafcd3
 #
-#curl -v https://y2pb2sdt6d.execute-api.us-west-2.amazonaws.com/test/demo/pathArg
-curl -v https://y2pb2sdt6d.execute-api.us-west-2.amazonaws.com/test/demo/404
+#curl -v https://5g21s17vke.execute-api.us-west-2.amazonaws.com/test/demo/pathArg
+#curl -v https://5g21s17vke.execute-api.us-west-2.amazonaws.com/test/demo/404
+#curl -v -H "Content-Type:application/json" -d '{"key":"value"}' https://5g21s17vke.execute-api.us-west-2.amazonaws.com/test/demo/pathArg
+curl -v -H "Content-Type:application/json" -d '{"key":"value"}' https://5g21s17vke.execute-api.us-west-2.amazonaws.com/test/demo/404
 #
